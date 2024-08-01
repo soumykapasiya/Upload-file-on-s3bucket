00dc8f4967fe466767f7331e1f3866a45e13b621
soyamkapasiya <163526543+soyamkapasiya@users.noreply.github.com>
2024-08-01 12:26:42 +0530

    01/08/2024

---
+++ src/main/java/com/kapasiya/uploadfileons3bucket/controller/StorageController.java
@@ -0,0 +1,39 @@
+package com.kapasiya.uploadfileons3bucket.controller;
+
+import com.kapasiya.uploadfileons3bucket.service.StorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
+public class StorageController {
+
+    private final StorageService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-Type", "application/octet-stream")
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/kapasiya/uploadfileons3bucket/UploadFileOnS3bucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapasiya.uploadfileons3bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileOnS3bucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
